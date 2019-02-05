d8ce570c5a75cd98732c81be84f112d952e9c1f2
Brandon Liu <bliu88@live.com>
2019-02-05 00:25:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(i)
+    if i % 3 == 0 && i % 5 == 0 
+      puts "Fizzbuzz"
+    elsif i % 3 == 0 
+      puts "Fizz"
+    elsif i % 5 == 0 
+      puts "Buzz"
+    else 
+      puts i 
+  end
+end
